1d17e04ff63fe191406dcb7ddd24f2ee27fdfc53
LazyManish <mbhattarai900@gmail.com>
2025-02-19 10:11:47 +0545

    small changes in response of delete method

--- Room/api/views.py
+++ Room/api/views.py
@@ -71,7 +71,7 @@ class RoomAPIView(APIView):
 
             room = Room.objects.get(pk=pk, admin=admin)  
             room.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({"message":"Room is deleted sucessfully."},status=status.HTTP_204_NO_CONTENT)
         
         except Exception as e:
             return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
